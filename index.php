e68089dfd6c5fdd561faf0c81888aca58d15a919
svDeveloper6 <developer.sv6@outlook.com>
2022-05-05 17:00:43 +0530

    I love css

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Hello</title>
+<link href='style.css' rel='stylesheet'>
+</head>
+<body>
+<p class="hello">lorem ipsum sit det amet , lorem up
+dekfh kedfyhjkewf ueiwtweb 
+I want to learn  ssh  scripting
+</p>
+</body>
+</html>
+
